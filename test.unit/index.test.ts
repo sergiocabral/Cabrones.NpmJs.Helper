2581f253016e3605d527169bb714a392d67c2302
Sergio Cabral <git@sergiocabral.com>
2021-07-19 03:09:06 -0300

    Jest test executando sem erros.

---
+++ test.unit/index.test.ts
@@ -0,0 +1,6 @@
+import helloWord from '../ts/index';
+
+test('teste', () => {
+    const result = helloWord();
+    expect(result).toBe('Hello World, but not implemented');
+});
--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,5 @@
-function helloWord() {
+//https://itnext.io/step-by-step-building-and-publishing-an-npm-typescript-package-44fe7164964c
+export default function helloWord(): string {
   const message: string = 'Hello World, but not implemented';
+  return message;
 }
-module.exports = helloWord;
