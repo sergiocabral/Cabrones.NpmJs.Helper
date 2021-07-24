40c9053e6e9b67ffd19658664e689a38673aa0cd
Sergio Cabral <git@sergiocabral.com>
2021-07-23 23:12:47 -0300

    UnitTest: HelperList.getRandom

--- jest.config.ts
+++ jest.config.ts
@@ -175,7 +175,7 @@ export default {
 
   // A map from regular expressions to paths to transformers
   "transform": {
-    "^.+\\.(t|j)sx?$": "ts-jest"
+    "^.+\\.tsx?$": "ts-jest"
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
