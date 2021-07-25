4f2a3fae54777265032a40efbfef0b83c9252dc7
Sergio Cabral <git@sergiocabral.com>
2021-07-25 01:17:01 -0300

    Incluído exports no index.

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,53 @@
+import { EmptyError } from './Error/EmptyError';
+import { GenericError } from './Error/GenericError';
+import { InvalidArgumentError } from './Error/InvalidArgumentError';
+import { InvalidDataError } from './Error/InvalidDataError';
+import { InvalidExecutionError } from './Error/InvalidExecutionError';
+import { InvalidUserOperationError } from './Error/InvalidUserOperationError';
+import { IOError } from './Error/IOError';
+import { NetworkError } from './Error/NetworkError';
+import { NotEmptyError } from './Error/NotEmptyError';
+import { NotImplementedError } from './Error/NotImplementedError';
+import { NotReadyError } from './Error/NotReadyError';
+import { RequestError } from './Error/RequestError';
+import { ShouldNeverHappenError } from './Error/ShouldNeverHappenError';
 import { HelperList } from './Helper/HelperList';
+import { HelperObject } from './Helper/HelperObject';
 import { HelperText } from './Helper/HelperText';
+import { ILogMessage } from './Log/ILogMessage';
+import { ILogWriter } from './Log/ILogWriter';
+import { Logger } from './Log/Logger';
+import { LogLevel } from './Log/LogLevel';
+import { LogWriter } from './Log/LogWriter';
+import { LogWriterToConsole } from './Log/LogWriterToConsole';
+import { KeyValue } from './Type/KeyValue';
 import './Helper/Prototype/String';
 
-export { HelperList, HelperText };
+export {
+  EmptyError,
+  GenericError,
+  InvalidArgumentError,
+  InvalidDataError,
+  InvalidExecutionError,
+  InvalidUserOperationError,
+  IOError,
+  NetworkError,
+  NotEmptyError,
+  NotImplementedError,
+  NotReadyError,
+  RequestError,
+  ShouldNeverHappenError,
+
+  HelperList,
+  HelperObject,
+  HelperText,
+
+  ILogMessage,
+  ILogWriter,
+  Logger,
+  LogLevel,
+  LogWriter,
+  LogWriterToConsole,
+
+  KeyValue
+};
