8cdc0a5aa0e170e371eaa34f31a9301bb1e2cf10
Sergio Cabral <git@sergiocabral.com>
2022-03-29 14:13:56 -0300

    test: Classes para CommandLine

--- test.unit/IO/CommandLineConfiguration.test.ts
+++ test.unit/IO/CommandLineConfiguration.test.ts
@@ -1,319 +1,309 @@
-import {CommandLineConfiguration, InvalidArgumentError} from '../../ts';
+import { CommandLineConfiguration, InvalidArgumentError } from '../../ts';
 
 describe('CommandLineConfiguration', () => {
-    describe('Estáticos', () => {
-        test('regexQuotes retorna RegExp corresponde às aspas informadas', () => {
-            // Arrange, Given
+  describe('Estáticos', () => {
+    test('regexQuotes retorna RegExp corresponde às aspas informadas', () => {
+      // Arrange, Given
 
-            const quotes: [string, string] = ["((", "]]]"];
-            const textMatched = "aqui ((match]]] sim";
-            const textNotMatched = "aqui ((match]] sim";
+      const quotes: [string, string] = ['((', ']]]'];
+      const textMatched = 'aqui ((match]]] sim';
+      const textNotMatched = 'aqui ((match]] sim';
 
-            // Act, When
+      // Act, When
 
-            const regex = CommandLineConfiguration.regexQuotes(quotes);
-            const textMatchedResult = regex.test(textMatched);
-            const textNotMatchedResult = regex.test(textNotMatched);
+      const regex = CommandLineConfiguration.regexQuotes(quotes);
+      const textMatchedResult = regex.test(textMatched);
+      const textNotMatchedResult = regex.test(textNotMatched);
 
-            // Assert, Then
+      // Assert, Then
 
-            expect(textMatchedResult).toBe(true);
-            expect(textNotMatchedResult).toBe(false);
-        });
-    })
-    describe('Valores padrão', () => {
-        test('caseInsensitiveForName', () => {
-            // Arrange, Given
-
-            const expectedValue = false;
-
-            // Act, When
-
-            const sut = new CommandLineConfiguration();
-
-            // Assert, Then
-
-            expect(sut.caseInsensitiveForName).toBe(expectedValue);
-        });
-        test('caseInsensitiveForValue', () => {
-            // Arrange, Given
-
-            const expectedValue = false;
-
-            // Act, When
-
-            const sut = new CommandLineConfiguration();
-
-            // Assert, Then
-
-            expect(sut.caseInsensitiveForValue).toBe(expectedValue);
-        });
-        test('attribution', () => {
-            // Arrange, Given
-
-            const expectedValue = '=';
-
-            // Act, When
-
-            const sut = new CommandLineConfiguration();
-
-            // Assert, Then
-
-            expect(sut.attribution).toBe(expectedValue);
-        });
-        test('quotes', () => {
-            // Arrange, Given
-
-            const expectedValue: Array<[string, string]> = [
-                ['"', '"'],
-                ["'", "'"],
-                ['`', '`'],
-                ['´', '´']
-            ];
-
-            // Act, When
-
-            const sut = new CommandLineConfiguration();
-
-            // Assert, Then
-
-            expect(sut.quotes.length).toBe(expectedValue.length);
-            for (let i = 0; i < sut.quotes.length; i++) {
-                expect(sut.quotes[i]).toStrictEqual(expectedValue[i]);
-            }
-        });
+      expect(textMatchedResult).toBe(true);
+      expect(textNotMatchedResult).toBe(false);
     });
-    describe('Informar propriedades no construtor da classe', () => {
-        test('caseInsensitiveForName', () => {
-            // Arrange, Given
+  });
+  describe('Valores padrão', () => {
+    test('caseInsensitiveForName', () => {
+      // Arrange, Given
 
-            const expectedValue = true;
+      const expectedValue = false;
 
-            // Act, When
+      // Act, When
 
-            const sut = new CommandLineConfiguration({
-                caseInsensitiveForName: expectedValue
-            });
+      const sut = new CommandLineConfiguration();
 
-            // Assert, Then
+      // Assert, Then
 
-            expect(sut.caseInsensitiveForName).toBe(expectedValue);
-        });
-        test('caseInsensitiveForValue', () => {
-            // Arrange, Given
-
-            const expectedValue = true;
-
-            // Act, When
-
-            const sut = new CommandLineConfiguration({
-                caseInsensitiveForValue: expectedValue
-            });
-
-            // Assert, Then
-
-            expect(sut.caseInsensitiveForValue).toBe(expectedValue);
-        });
-        test('attribution', () => {
-            // Arrange, Given
-
-            const expectedValue = Math.random().toString();
-
-            // Act, When
-
-            const sut = new CommandLineConfiguration({
-                attribution: expectedValue
-            });
-
-            // Assert, Then
-
-            expect(sut.attribution).toBe(expectedValue);
-        });
-        test('quotes', () => {
-            // Arrange, Given
-
-            const expectedValue: Array<[string, string]> = [
-                ['"', '"']
-            ];
-
-            // Act, When
-
-            const sut = new CommandLineConfiguration({
-                quotes: expectedValue
-            });
-
-            // Assert, Then
-
-            expect(sut.quotes).toStrictEqual(expectedValue);
-        });
-    })
-    describe('Deve poder modificar as propriedades', () => {
-        test('caseInsensitiveForName', () => {
-            // Arrange, Given
-
-            const sut = new CommandLineConfiguration();
-            const initialValue = sut.caseInsensitiveForName;
-            const newValue = !initialValue
-
-            // Act, When
-
-            sut.caseInsensitiveForName = newValue;
-
-            // Assert, Then
-
-            expect(sut.caseInsensitiveForName).not.toBe(initialValue);
-            expect(sut.caseInsensitiveForName).toBe(newValue);
-        });
-        test('caseInsensitiveForValue', () => {
-            // Arrange, Given
-
-            const sut = new CommandLineConfiguration();
-            const initialValue = sut.caseInsensitiveForValue;
-            const newValue = !initialValue
-
-            // Act, When
-
-            sut.caseInsensitiveForValue = newValue;
-
-            // Assert, Then
-
-            expect(sut.caseInsensitiveForValue).not.toBe(initialValue);
-            expect(sut.caseInsensitiveForValue).toBe(newValue);
-        });
-        test('attribution', () => {
-            // Arrange, Given
-
-            const sut = new CommandLineConfiguration();
-            const initialValue = sut.attribution;
-            const newValue = Math.random().toString();
-
-            // Act, When
-
-            sut.attribution = newValue;
-
-            // Assert, Then
-
-            expect(sut.attribution).not.toBe(initialValue);
-            expect(sut.attribution).toBe(newValue);
-        });
-        test('quotes', () => {
-            // Arrange, Given
-
-            const sut = new CommandLineConfiguration();
-            const initialValue = sut.quotes;
-            const newValue: Array<[string, string]> = [
-                ['[', ']'],
-                ['<', '>']
-            ];
-
-            // Act, When
-
-            sut.quotes = newValue;
-
-            // Assert, Then
-
-            expect(sut.quotes).not.toBe(initialValue);
-            expect(sut.quotes).toStrictEqual(newValue);
-        });
+      expect(sut.caseInsensitiveForName).toBe(expectedValue);
     });
-    describe('Validação de valores', () => {
-        test('attribution não pode ser vazio', () => {
-            // Arrange, Given
+    test('caseInsensitiveForValue', () => {
+      // Arrange, Given
 
-            const sut = new CommandLineConfiguration();
-            const emptyValue = '';
+      const expectedValue = false;
 
-            // Act, When
+      // Act, When
 
-            const action = () => sut.attribution = emptyValue;
+      const sut = new CommandLineConfiguration();
 
-            // Assert, Then
+      // Assert, Then
 
-            expect(action).toThrow(InvalidArgumentError);
-        });
-        test('quotes não pode ser lista vazia', () => {
-            // Arrange, Given
-
-            const sut = new CommandLineConfiguration();
-            const emptyValue: Array<[string, string]> = [];
-
-            // Act, When
-
-            const action = () => sut.quotes = emptyValue;
-
-            // Assert, Then
-
-            expect(action).toThrow(InvalidArgumentError);
-        });
-        test('quotes, aspa esquerda não pode ser vazia', () => {
-            // Arrange, Given
-
-            const sut = new CommandLineConfiguration();
-            const invalidValue: Array<[string, string]> = [
-                ['', '>']
-            ];
-
-            // Act, When
-
-            const action = () => sut.quotes = invalidValue;
-
-            // Assert, Then
-
-            expect(action).toThrow(InvalidArgumentError);
-        });
-        test('quotes, aspa direita não pode ser vazia', () => {
-            // Arrange, Given
-
-            const sut = new CommandLineConfiguration();
-            const invalidValue: Array<[string, string]> = [
-                ['<', '']
-            ];
-
-            // Act, When
-
-            const action = () => sut.quotes = invalidValue;
-
-            // Assert, Then
-
-            expect(action).toThrow(InvalidArgumentError);
-        });
+      expect(sut.caseInsensitiveForValue).toBe(expectedValue);
     });
-    describe('Testes para removeQuotes', () => {
-        test('Remover todas as aspas de um texto', () => {
-            // Arrange, Given
+    test('attribution', () => {
+      // Arrange, Given
 
-            const sut = new CommandLineConfiguration({
-                quotes: [
-                    ["<!--", "-->"],
-                    ["/**", "*/"],
-                ]
-            });
+      const expectedValue = '=';
 
-            const comment = Math.random().toString();
-            const textWithQuotes = `<h1>Título <!--${comment}--> Principal</h1><script>/** ${comment} */</script>`;
-            const textWithoutQuotes = `<h1>Título ${comment} Principal</h1><script> ${comment} </script>`;
+      // Act, When
 
-            // Act, When
+      const sut = new CommandLineConfiguration();
 
-            const result = sut.removeQuotes(textWithQuotes);
+      // Assert, Then
 
-            // Assert, Then
+      expect(sut.attribution).toBe(expectedValue);
+    });
+    test('quotes', () => {
+      // Arrange, Given
 
-            expect(result).toBe(textWithoutQuotes);
-        });
-        test('', () => {
-            // Arrange, Given
+      const expectedValue: Array<[string, string]> = [
+        ['"', '"'],
+        ["'", "'"],
+        ['`', '`'],
+        ['´', '´']
+      ];
 
-            // Actm When
+      // Act, When
 
-            // Assert, Then
-        });
-        test('', () => {
-            // Arrange, Given
+      const sut = new CommandLineConfiguration();
 
-            // Actm When
+      // Assert, Then
 
-            // Assert, Then
-        });
-    })
+      expect(sut.quotes.length).toBe(expectedValue.length);
+      for (let i = 0; i < sut.quotes.length; i++) {
+        expect(sut.quotes[i]).toStrictEqual(expectedValue[i]);
+      }
+    });
+  });
+  describe('Informar propriedades no construtor da classe', () => {
+    test('caseInsensitiveForName', () => {
+      // Arrange, Given
+
+      const expectedValue = true;
+
+      // Act, When
+
+      const sut = new CommandLineConfiguration({
+        caseInsensitiveForName: expectedValue
+      });
+
+      // Assert, Then
+
+      expect(sut.caseInsensitiveForName).toBe(expectedValue);
+    });
+    test('caseInsensitiveForValue', () => {
+      // Arrange, Given
+
+      const expectedValue = true;
+
+      // Act, When
+
+      const sut = new CommandLineConfiguration({
+        caseInsensitiveForValue: expectedValue
+      });
+
+      // Assert, Then
+
+      expect(sut.caseInsensitiveForValue).toBe(expectedValue);
+    });
+    test('attribution', () => {
+      // Arrange, Given
+
+      const expectedValue = Math.random().toString();
+
+      // Act, When
+
+      const sut = new CommandLineConfiguration({
+        attribution: expectedValue
+      });
+
+      // Assert, Then
+
+      expect(sut.attribution).toBe(expectedValue);
+    });
+    test('quotes', () => {
+      // Arrange, Given
+
+      const expectedValue: Array<[string, string]> = [['"', '"']];
+
+      // Act, When
+
+      const sut = new CommandLineConfiguration({
+        quotes: expectedValue
+      });
+
+      // Assert, Then
+
+      expect(sut.quotes).toStrictEqual(expectedValue);
+    });
+  });
+  describe('Deve poder modificar as propriedades', () => {
+    test('caseInsensitiveForName', () => {
+      // Arrange, Given
+
+      const sut = new CommandLineConfiguration();
+      const initialValue = sut.caseInsensitiveForName;
+      const newValue = !initialValue;
+
+      // Act, When
+
+      sut.caseInsensitiveForName = newValue;
+
+      // Assert, Then
+
+      expect(sut.caseInsensitiveForName).not.toBe(initialValue);
+      expect(sut.caseInsensitiveForName).toBe(newValue);
+    });
+    test('caseInsensitiveForValue', () => {
+      // Arrange, Given
+
+      const sut = new CommandLineConfiguration();
+      const initialValue = sut.caseInsensitiveForValue;
+      const newValue = !initialValue;
+
+      // Act, When
+
+      sut.caseInsensitiveForValue = newValue;
+
+      // Assert, Then
+
+      expect(sut.caseInsensitiveForValue).not.toBe(initialValue);
+      expect(sut.caseInsensitiveForValue).toBe(newValue);
+    });
+    test('attribution', () => {
+      // Arrange, Given
+
+      const sut = new CommandLineConfiguration();
+      const initialValue = sut.attribution;
+      const newValue = Math.random().toString();
+
+      // Act, When
+
+      sut.attribution = newValue;
+
+      // Assert, Then
+
+      expect(sut.attribution).not.toBe(initialValue);
+      expect(sut.attribution).toBe(newValue);
+    });
+    test('quotes', () => {
+      // Arrange, Given
+
+      const sut = new CommandLineConfiguration();
+      const initialValue = sut.quotes;
+      const newValue: Array<[string, string]> = [
+        ['[', ']'],
+        ['<', '>']
+      ];
+
+      // Act, When
+
+      sut.quotes = newValue;
+
+      // Assert, Then
+
+      expect(sut.quotes).not.toBe(initialValue);
+      expect(sut.quotes).toStrictEqual(newValue);
+    });
+  });
+  describe('Validação de valores', () => {
+    test('attribution não pode ser vazio', () => {
+      // Arrange, Given
+
+      const sut = new CommandLineConfiguration();
+      const emptyValue = '';
+
+      // Act, When
+
+      const action = () => (sut.attribution = emptyValue);
+
+      // Assert, Then
+
+      expect(action).toThrow(InvalidArgumentError);
+    });
+    test('quotes não pode ser lista vazia', () => {
+      // Arrange, Given
+
+      const sut = new CommandLineConfiguration();
+      const emptyValue: Array<[string, string]> = [];
+
+      // Act, When
+
+      const action = () => (sut.quotes = emptyValue);
+
+      // Assert, Then
+
+      expect(action).toThrow(InvalidArgumentError);
+    });
+    test('quotes, aspa esquerda não pode ser vazia', () => {
+      // Arrange, Given
+
+      const sut = new CommandLineConfiguration();
+      const invalidValue: Array<[string, string]> = [['', '>']];
+
+      // Act, When
+
+      const action = () => (sut.quotes = invalidValue);
+
+      // Assert, Then
+
+      expect(action).toThrow(InvalidArgumentError);
+    });
+    test('quotes, aspa direita não pode ser vazia', () => {
+      // Arrange, Given
+
+      const sut = new CommandLineConfiguration();
+      const invalidValue: Array<[string, string]> = [['<', '']];
+
+      // Act, When
+
+      const action = () => (sut.quotes = invalidValue);
+
+      // Assert, Then
+
+      expect(action).toThrow(InvalidArgumentError);
+    });
+  });
+  describe('Testes para removeQuotes', () => {
+    test('Remover todas as aspas de um texto', () => {
+      // Arrange, Given
+
+      const sut = new CommandLineConfiguration({
+        quotes: [
+          ['<!--', '-->'],
+          ['/**', '*/']
+        ]
+      });
+
+      const comment = Math.random().toString();
+      const textWithQuotes = `<h1>Título <!--${comment}--> Principal</h1><script>/** ${comment} */</script>`;
+      const textWithoutQuotes = `<h1>Título ${comment} Principal</h1><script> ${comment} </script>`;
+
+      // Act, When
+
+      const result = sut.removeQuotes(textWithQuotes);
+
+      // Assert, Then
+
+      expect(result).toBe(textWithoutQuotes);
+    });
+    test('', () => {
+      // Arrange, Given
+      // Actm When
+      // Assert, Then
+    });
+    test('', () => {
+      // Arrange, Given
+      // Actm When
+      // Assert, Then
+    });
+  });
 });
